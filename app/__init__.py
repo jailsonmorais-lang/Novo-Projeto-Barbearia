a18f3c4c764d542f4618f997fd7bab13ec8c5807
JailsonMorais <jailson.o.a.morais@gmail.com>
2026-03-27 20:37:39 -0300

    fix: corrige HTTPS no OAuth Google em produção

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,20 @@
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 import os
+
 
 def create_app():
     if os.getenv('FLASK_ENV') != 'production':
         os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+        os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+    
     # Aqui o Flask entende que deve olhar para dentro da pasta 'app'
     app = Flask(__name__,
                 instance_relative_config=True,
                 template_folder='templates',
                 static_folder='static',
                 )
+    app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
     app.secret_key = os.getenv('SECRET_KEY')
     
     # Importa as rotas (Isso evita erros de importação circular)
